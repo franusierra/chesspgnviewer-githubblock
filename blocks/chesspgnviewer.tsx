3d9d5b0aaeef79c5d9ff23aee888696a6325abcc
frnunezs182 <franusierra@gmail.com>
2023-10-19 23:31:21 +0200

    Add way to update pgn

--- blocks/chesspgnviewer.tsx
+++ blocks/chesspgnviewer.tsx
@@ -1,10 +1,12 @@
 import { FileBlockProps, getLanguageFromFilename } from "@githubnext/blocks";
 import { pgnEdit, pgnView } from "@mliebelt/pgn-viewer";
+import { writeGame } from './pgn-writer/pgn-writer';
 import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { Button } from "@primer/react"; 
 import { Layout } from "@mliebelt/pgn-viewer/lib/types";
 
 export default function ExampleFileBlock(props: FileBlockProps) {
-  const { context, content, metadata, onUpdateMetadata } = props;
+  const { context, content, metadata, onUpdateMetadata,onUpdateContent } = props;
   const language = Boolean(context.path)
     ? getLanguageFromFilename(context.path)
     : "N/A";
@@ -49,9 +51,20 @@ export default function ExampleFileBlock(props: FileBlockProps) {
       manyGames: true,
       showFen: false
     });
+    console.log(board);
+    console.log(base);
+    
     currentBase.current=base;
   }, [windowSize]);
-
+  function saveCurrentGame(){
+    let pgnReader : any= currentBase.current.getPgn();
+    let editedCurrentPgnContent="";
+    for(let i=0;i<pgnReader.getGames().length;i++){
+      let game=pgnReader.getGame(i);
+      editedCurrentPgnContent+=writeGame(game, pgnReader.configuration)+"\n\n";
+    }
+    onUpdateContent(editedCurrentPgnContent);
+  }
   return (
     <div
       style={{
@@ -63,6 +76,7 @@ export default function ExampleFileBlock(props: FileBlockProps) {
       }}
     >
       <div id="board" style={{ margin: "auto" }} />
+      <Button style={{position:'absolute',top:'8px',left:'8px'}} onClick={saveCurrentGame}>Save current game</Button>
     </div>
   );
 }
---
+++ blocks/pgn-writer/types.ts
@@ -0,0 +1,55 @@
+export const PROMOTIONS = {
+    'q': 'queen',
+    'r': 'rook',
+    'b': 'bishop',
+    'n': 'knight'
+}
+
+export const prom_short = ['q', 'r', 'b', 'n']
+export type PROMOTIONS_SHORT = typeof prom_short[number]
+
+export const colors = ['white', 'black'] as const
+export const files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'] as const
+export const ranks = ['1', '2', '3', '4', '5', '6', '7', '8'] as const
+
+export type File = typeof files[number]
+export type Rank = typeof ranks[number]
+export type Field = 'a0' | `${File}${Rank}`
+
+export type GameComment = { comment?: string, colorArrows?: string[], colorFields?: string[], clk?: string, eval?: string }
+export type Color = 'w' | 'b'
+
+export type ConfigurationTagsValues = "no" | "7r" | "known" | "all"
+export type PgnWriterConfiguration = {
+    tags?: ConfigurationTagsValues,
+    notation?: string
+}
+
+export type PgnReaderMove = {
+    drawOffer?: boolean;
+    moveNumber?: number,
+    notation: { fig?: string | null, strike?: 'x' | null, col?: string, row?: string, check?: string, ep?: boolean
+        promotion?: string | null, notation: string, disc?: string, drop?: boolean },
+    variations: PgnReaderMove[],
+    nag: string[],
+    commentDiag?: GameComment,
+    commentMove?: string,
+    commentAfter?: string,
+    turn?: Color
+    from: Field,
+    to: Field,
+    fen?: string,
+    index?: number,
+    prev?: number,
+    next?: number,
+    variationLevel?: number
+}
+
+export type SevenRoosterTagKeys = 'Event'|'Site'|'Round'|'White'|'Black'|'Result|Date'
+export type Tags = { string: string }
+
+export type PgnGame = {
+    tags?: Tags,
+    gameComment?: GameComment,
+    moves: PgnReaderMove[]
+}
