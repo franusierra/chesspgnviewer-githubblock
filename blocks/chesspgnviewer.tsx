3bc637d0a72fa43d18aabf71280803af9f726e99
frnunezs182 <franusierra@gmail.com>
2023-10-19 21:52:21 +0200

    feat: add support for resizing window and a couple more games

--- blocks/chesspgnviewer.tsx
+++ blocks/chesspgnviewer.tsx
@@ -1,10 +1,7 @@
 import { FileBlockProps, getLanguageFromFilename } from "@githubnext/blocks";
-import "./index.css";
-import { pgnView } from "@mliebelt/pgn-viewer";
-import { PgnReader} from "@mliebelt/pgn-reader";
+import { pgnEdit, pgnView } from "@mliebelt/pgn-viewer";
 import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { Layout } from "@mliebelt/pgn-viewer/lib/types";
-
 
 export default function ExampleFileBlock(props: FileBlockProps) {
   const { context, content, metadata, onUpdateMetadata } = props;
@@ -23,38 +20,49 @@ export default function ExampleFileBlock(props: FileBlockProps) {
         width: window.innerWidth,
         height: window.innerHeight,
       });
-      console.log(windowSize)
+      console.log(windowSize);
     }
-    
+
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
-
+  let currentBase=useRef<any>();
   useLayoutEffect(() => {
     // Only way to make responsiveness (on resizing resets the board but haven't found a better way)
-    let boardSize = `${windowSize.height-192}px`;
-    let layout:Layout = "left";
-      if(windowSize.width<600){
-        boardSize = `${windowSize.width-40}px`;
-        layout = "top";
-      }
-      let {base,board}=pgnView("board", {
-        pgn: content,
-        pieceStyle: "wikipedia",
-        theme: "brown",
-        showResult: true,
-        boardSize: boardSize,
-        layout: layout,
-        figurine: "merida",
-        manyGames: true,
-      });
-      
+    let boardSize = `${windowSize.height - 400}px`;
+    let layout: Layout = "left";
+    let width = `${windowSize.width*2/3}px`;
+    if (windowSize.width < 600) {
+      boardSize = `${windowSize.width - 40}px`;
+      width = boardSize;
+      layout = "top";
+    }
+    let { base, board } = pgnEdit("board", {
+      pgn: currentBase.current?currentBase.current.getPgn().writePgn():content,
+      pieceStyle: "wikipedia",
+      theme: "brown",
+      showResult: true,
+      boardSize: boardSize,
+      width: width,
+      layout: layout,
+      figurine: "merida",
+      manyGames: true,
+      showFen: false
+    });
+    currentBase.current=base;
   }, [windowSize]);
-  
+
   return (
-    <div style={{width:"100%",height:"100%",display:"flex",justifyContent: "center", alignItems: "center"}}>
-      <div id="board" style={{margin:"auto"}} />
-      
-    </div>  
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div id="board" style={{ margin: "auto" }} />
+    </div>
   );
 }
